398823c64b0f7220331f42202b1c6c337cef884a
Ahmed Nasser <ahmed.nasser2310@gmail.com>
2019-12-06 14:58:46 +0200

    no message

--- iOS Clean Architecture/Network/NetworkConstants.swift
+++ iOS Clean Architecture/Network/NetworkConstants.swift
@@ -12,7 +12,7 @@ enum SearchMethod :String{
     case groups = "flickr.groups.search"
 }
 struct APIConstant {
-    static let flickerApiKey = "77b714cd4503f9b42968c0110fb1b15c"
+    static let flickerApiKey = "API KEY"
     static let baseURL = "https://api.flickr.com/services/rest/"
 }
 struct Environments {
